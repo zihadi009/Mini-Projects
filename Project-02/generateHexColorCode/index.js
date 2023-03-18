a5f3a2ea50f7e6a3933f849a45a931c50dea5da2
Musfiqur Rahman Zihad <musfiqurahmanzihad@gmail.com>
2023-03-18 20:40:17 +0600

    Add files via upload

---
+++ Project-02/generateHexColorCode/index.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    main();
+}
+
+function main() {
+    const root = document.getElementById('root');
+    const btn = document.getElementById('change-btn');
+    const output = document.getElementById('output');
+
+    btn.addEventListener('click',function(){
+        const bgColor = generateHexColorCode();
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    })
+
+}
+
+function generateHexColorCode() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
